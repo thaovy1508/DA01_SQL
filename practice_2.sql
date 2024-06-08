9c1ecc5d948ca0a9f13e67bbe85f111ca14ba619
Vy Nguyen <75440942+thaovy1508@users.noreply.github.com>
2024-06-08 11:21:11 -0400

    Update practice_2.sql

--- practice_2.sql
+++ practice_2.sql
@@ -1,11 +1,13 @@
 #Exercise 1
-select distinct city from station
-where ID % 2 = 0;
+SELECT distinct city FROM station
+WHERE ID % 2 = 0;
 
 #Exercise 2
-select count(city) - count (distinct city) from station;
+SELECT count(city) - count (distinct city) FROM station;
 
 #Exercise 3
-select ceil(avg(salary) - avg(replace(salary,0,''))) from Employees;
+SELECT ceil(avg(salary) - avg(replace(salary,0,''))) FROM Employees;
 
-#Exercise 3
+#Exercise 4
+SELECT round(CAST(sum(order_occurrences*item_count)/sum(order_occurrences) AS numeric), 1) 
+  FROM items_per_order;

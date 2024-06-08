38a09b02effee5f17e6b5f248a4480d7a91c1522
Vy Nguyen <75440942+thaovy1508@users.noreply.github.com>
2024-06-08 11:30:22 -0400

    Update practice_2.sql

--- practice_2.sql
+++ practice_2.sql
@@ -11,3 +11,10 @@ SELECT ceil(avg(salary) - avg(replace(salary,0,''))) FROM Employees;
 #Exercise 4
 SELECT round(CAST(sum(order_occurrences*item_count)/sum(order_occurrences) AS numeric), 1) 
   FROM items_per_order;
+
+#Exercise 5
+SELECT candidate_id FROM candidates
+WHERE skill in ('Python','Tableau','PostgreSQL')
+GROUP BY candidate_id
+HAVING count(skill) = 3
+ORDER BY candidate_id asc;

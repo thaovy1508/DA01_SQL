f3f52847cceadf53c9d825e50fda20b57c80ed2c
Vy Nguyen <75440942+thaovy1508@users.noreply.github.com>
2024-06-08 12:17:47 -0400

    Update practice_2.sql

--- practice_2.sql
+++ practice_2.sql
@@ -18,3 +18,42 @@ WHERE skill in ('Python','Tableau','PostgreSQL')
 GROUP BY candidate_id
 HAVING count(skill) = 3
 ORDER BY candidate_id asc;
+
+#Exercise 6
+SELECT user_id, 
+max(post_date::date)-min(post_date::date) AS days_between 
+FROM posts
+WHERE EXTRACT(YEAR from post_date) = '2021'
+GROUP BY user_id
+HAVING count(post_id) > 1;
+
+#Exercise 7
+SELECT card_name, max(issued_amount)-min(issued_amount) AS difference
+FROM monthly_cards_issued
+GROUP BY card_name
+ORDER BY difference DESC;
+
+#Exercise 8
+SELECT manufacturer, 
+COUNT(1) AS drug_count, 
+SUM(cogs)-SUM(total_sales) AS total_loss 
+FROM pharmacy_sales
+WHERE cogs > total_sales
+GROUP BY manufacturer
+ORDER BY total_loss DESC;
+
+#Exercise 9
+SELECT * FROM Cinema
+WHERE id%2 =1
+AND description != 'boring'
+ORDER BY rating DESC;
+
+#Exercise 10
+SELECT teacher_id, count(distinct subject_id) AS cnt
+FROM Teacher
+GROUP BY teacher_id;
+
+#Exercise 11
+
+
+#Exercise 12

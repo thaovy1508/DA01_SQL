bf8e1a010f19622e138dc3233ef608295d097879
Vy Nguyen <75440942+thaovy1508@users.noreply.github.com>
2024-06-13 11:28:39 -0400

    Update practice_5.sql

--- practice_5.sql
+++ practice_5.sql
@@ -6,3 +6,13 @@ ON a.CountryCode = b.Code
 GROUP BY b.Continent;
 
 #Exercise 2
+SELECT round(cast(sum(
+case 
+  when t.signup_action = 'Confirmed' then 1 
+  else 0 
+end) as decimal)/count(e.email_id),2) as confirm_rate 
+FROM emails e
+JOIN texts t
+ON t.email_id = e.email_id;
+
+#Exercise 3

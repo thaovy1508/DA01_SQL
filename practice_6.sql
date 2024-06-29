2a485f5da3646eccd20a2cabe1caba1a440e4269
Vy Nguyen <75440942+thaovy1508@users.noreply.github.com>
2024-06-29 13:30:25 -0400

    Update practice_6.sql

--- practice_6.sql
+++ practice_6.sql
@@ -8,3 +8,28 @@ FROM cn_dup
 WHERE cnt > 1
 
 #Exercise 2
+WITH 
+appliance_cte AS (
+SELECT category, product, SUM(spend) AS total_spend
+FROM product_spend
+WHERE category='appliance'
+AND EXTRACT(year FROM transaction_date) = 2022
+GROUP BY category, product
+ORDER BY total_spend DESC
+LIMIT 2),
+
+electronics_cte AS (
+SELECT category, product, SUM(spend) AS total_spend
+FROM product_spend
+WHERE category='electronics'
+AND EXTRACT(year FROM transaction_date) = 2022
+GROUP BY category, product
+ORDER BY total_spend DESC
+LIMIT 2)
+
+SELECT * FROM appliance_cte 
+UNION 
+SELECT * FROM electronics_cte
+ORDER BY category ASC, total_spend DESC
+
+#Exercise 3

83a17f314b74b2faa65537ebd59e9c752c61d5de
Vy Nguyen <75440942+thaovy1508@users.noreply.github.com>
2024-06-29 13:08:07 -0400

    Update practice_6.sql

--- practice_6.sql
+++ practice_6.sql
@@ -1,1 +1,10 @@
+#Exercise 1
+WITH cn_dup AS (
+SELECT title, count(*) AS cnt FROM job_listings
+GROUP BY company_id, title, description)
 
+SELECT count(*) as duplicate_companies
+FROM cn_dup
+WHERE cnt > 1
+
+#Exercise 2

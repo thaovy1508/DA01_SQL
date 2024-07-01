56f40c4729581f7eb47c6181eef5a83126d1d231
Vy Nguyen <75440942+thaovy1508@users.noreply.github.com>
2024-06-30 21:32:57 -0400

    Update practice_6.sql

--- practice_6.sql
+++ practice_6.sql
@@ -54,6 +54,14 @@ FROM (
 ) AS call_records;
 
 #Exercise 4
+SELECT p.page_id 
+FROM pages p
+FULL OUTER JOIN page_likes l
+ON p.page_id = l.page_id
+WHERE l.user_id IS NULL
+ORDER BY p.page_id asc;
+  
+#Exercise 5
 SELECT EXTRACT(MONTH from event_date),
 COUNT(DISTINCT user_id)
 FROM user_actions
@@ -62,7 +70,7 @@ AND user_id IN (SELECT user_id FROM user_actions WHERE EXTRACT(MONTH from event_
 GROUP BY 1
 ORDER BY 1,2
 
-#Exercise 5
+#Exercise 6
 SELECT 
     DATE_FORMAT(trans_date, '%Y-%m') AS month, 
     country,
@@ -73,6 +81,99 @@ SELECT
 FROM Transactions
 GROUP BY month, country
 
-#Exercise 6
+#Exercise 7
+#Method 1 - subquery
+SELECT product_id, year as first_year, quantity, price
+FROM Sales
+WHERE (product_id, year) in (
+    SELECT product_id, MIN(year)
+    FROM Sales
+    GROUP BY product_id
+)
+
+#Method 2 - cte
+WITH first_year_sale_cte AS(
+    SELECT s.product_id,
+        MIN(s.year) AS first_year
+    FROM Sales s
+    GROUP BY s.product_id)
+
+SELECT 
+    fys.product_id, 
+    fys.first_year,
+    s.quantity,
+    s.price
+FROM first_year_sale_cte fys
+JOIN Sales s ON fys.product_id = s.product_id 
+AND fys.first_year = s.year
+
+#Exercise 8
+SELECT customer_id
+FROM Customer c
+JOIN Product p
+ON c.product_key = p.product_key
+GROUP BY c.customer_id
+HAVING COUNT(DISTINCT c.product_key) = 
+(SELECT COUNT(DISTINCT product_key) 
+FROM Product)
+
+#Exercise 9
+# Method 1 - cte
+WITH employees_cte AS (
+SELECT employee_id, manager_id
+FROM employees 
+WHERE salary < 30000)
+
+SELECT e.employee_id
+FROM employees_cte e
+WHERE e.manager_id NOT IN (SELECT employee_id FROM employees)
+
+# Method 2 - correlated
+SELECT employee_id
+FROM employees 
+WHERE manager_id NOT IN (SELECT employee_id FROM employees)
+AND salary < 30000
+
+#Exercise 10
+SELECT 
+    employee_id,
+    CASE 
+        WHEN count(*) = 1 THEN MAX(department_id)
+        WHEN count(*) > 1 THEN MAX(CASE WHEN primary_flag = 'Y' THEN department_id END)
+    END AS department_id
+FROM employee
+GROUP BY employee_id)
+
+SELECT employee_id, department_id
+FROM employee_cte
+WHERE department_id IS NOT NULL
+
+#Exercise 11
+WITH mv_cte AS (
+SELECT m.user_id, count(DISTINCT m.movie_id) AS num_of_rating
+FROM MovieRating m
+GROUP BY m.user_id) 
+
+(SELECT u.name AS results
+FROM mv_cte m
+JOIN Users u 
+ON u.user_id = m.user_id
+ORDER BY m.num_of_rating DESC, u.name ASC
+LIMIT 1)
+
+UNION ALL
+
+(SELECT m.title
+FROM MovieRating mr
+JOIN Movies m
+ON mr.movie_id = m.movie_id
+WHERE extract(YEAR from mr.created_at) = 2020
+AND extract(MONTH from mr.created_at) = 2
+GROUP BY mr.movie_id
+ORDER BY AVG(mr.rating) DESC, m.title ASC
+LIMIT 1)
+
+#Exercise 12
+
 
 

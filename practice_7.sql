98a057917ddd7ebd09d9756eb1127f05c12241c0
Vy Nguyen <75440942+thaovy1508@users.noreply.github.com>
2024-07-13 22:23:43 -0400

    Update practice_7.sql

--- practice_7.sql
+++ practice_7.sql
@@ -1,1 +1,22 @@
+#Exercise 1
+WITH yearly_spend_cte AS
+(SELECT 
+
+  EXTRACT(YEAR FROM transaction_date) AS year,
+  product_id,
+  spend AS curr_year_spend,
+  lag(spend) OVER(
+    PARTITION BY product_id 
+    ORDER BY 
+      product_id, 
+      EXTRACT(YEAR FROM transaction_date)) AS prev_year_spend
+      
+FROM user_transactions)
+
+SELECT 
+  year, product_id, curr_year_spend, prev_year_spend,
+  ROUND(100*(curr_year_spend-prev_year_spend)/prev_year_spend,2) AS yoy_rate 
+FROM yearly_spend_cte
+
+#Exercise 2
 

551d530845a2adaba2d49b0b22cc7d78bd6012e8
Vy Nguyen <75440942+thaovy1508@users.noreply.github.com>
2024-07-15 12:38:45 -0400

    Update practice_7.sql

--- practice_7.sql
+++ practice_7.sql
@@ -105,5 +105,16 @@ GROUP BY user_id, transaction_date
 ORDER BY transaction_date
   
 #Exercise 5
+SELECT    
+  user_id,    
+  tweet_date,   
+  ROUND(AVG(tweet_count) OVER (
+    PARTITION BY user_id     
+    ORDER BY tweet_date     
+    ROWS BETWEEN 2 PRECEDING AND CURRENT ROW) ,2) AS rolling_avg_3d
+FROM tweets;
+
+#Exercise 6
+
 
 

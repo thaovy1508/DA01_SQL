1bcc4cf283b21a6a36f552989ea91bba1820afb2
Vy Nguyen <75440942+thaovy1508@users.noreply.github.com>
2024-07-28 22:53:04 -0400

    Update project_2.sql

--- project_2.sql
+++ project_2.sql
@@ -48,38 +48,38 @@ from avg_cte
 
 
 -- 3. Output: first_name, last_name, gender, age, tag 
-      Insights: 
+      Insights: Youngest 12 years old, Female: 474, Male:490
+                Oldest: 70 years old, Female:477, Male: 501
 
-WITH order_cte as (
-select  o.order_id, 
-        o.user_id, 
-        i.sale_price,
-        u.first_name,
-        u.last_name,
-        u.gender,
-        u.age,
-        EXTRACT(YEAR from o.created_at) || '-' || EXTRACT(month from o.created_at) as month_year
-from bigquery-public-data.thelook_ecommerce.orders o
-left join bigquery-public-data.thelook_ecommerce.order_items i
-on i.order_id = o.order_id
-right join bigquery-public-data.thelook_ecommerce.users u
-on i.user_id = u.id
-where o.status='Complete'
-and DATE(o.created_at) between '2019-01-01' and '2022-04-30'),
+with cte as (
+  select gender, 
+          min(age) as min_age,
+          max(age) as max_age
+  from bigquery-public-data.thelook_ecommerce.users
+  group by gender
+),
+min_cte as (
+select id, a.gender, age, first_name, last_name
+from bigquery-public-data.thelook_ecommerce.users a
+join cte b
+on a.gender = b.gender
+where age = min_age
+and DATE(a.created_at) between '2019-01-01' and '2022-04-30'),
 
-avg_cte as (
-select month_year,
-      count(distinct user_id) as distinct_user,
-      count(distinct order_id) as total_order,
-      sum(sale_price) as total_price
-from order_cte
-group by 1
-order by 1)
+max_cte as (
+select id, a.gender, age, first_name, last_name
+from bigquery-public-data.thelook_ecommerce.users a
+join cte b
+on a.gender = b.gender
+where age = max_age
+and DATE(a.created_at) between '2019-01-01' and '2022-04-30')
 
-select month_year,
-       distinct_user,
-       total_price/total_order as average_order_value
-from avg_cte
+(select gender, count(*) as total ,'youngest' as tag
+from min_cte 
+group by gender)
 
-id, a.gender, age, first_name, last_name
+union all
 
+(select gender, count(*) as total, 'oldest' as tag
+from max_cte 
+group by gender)
